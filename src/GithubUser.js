3340ad92f1245057dbcc8d5c4bfe99a711384dc1
DarkTachyon <tmb.alexander@gmail.com>
2017-07-11 20:00:26 -0400

    Finish up github user section

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
 
+import './Github.css'
+
 class GithubUser extends Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            user: {}
+        }
 
         this.fetchUserData()
     }
@@ -10,12 +16,27 @@ class GithubUser extends Component {
     fetchUserData = () => {
         fetch(`https://api.github.com/users/${this.props.match.username}`)
             .then(response => response.json())
-            .then(user => console.log(user))
+            .then(user => this.setState({ user }))
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const locationChanged = nextProps.location !== this.props.location
+        if (locationChanged) {
+            this.fetchUserData()
+        }
     }
 
     render() {
+        const { user } = this.state
         return (
-            <h1>github user</h1>
+            <div className="github-user">
+                <img src={user.avatar_url} alt="github user avatar"/>
+                <h2>{user.login}</h2>
+                <h3>followers: {user.followers}</h3>
+                <h3>following: {user.following}</h3>
+                <h3>location: {user.location}</h3>
+                <a href={user.html_url} target="_">Link to {user.login} s profile</a>
+            </div>
         )
     }
 }
